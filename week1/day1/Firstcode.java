7bea2808f59d107d8ce917a4650e9582ec1938b1
sangeekevin <134406925+sangeekevin@users.noreply.github.com>
2023-05-26 14:17:17 +0530

    Add files via upload

---
+++ week1/day1/Firstcode.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class Firstcode {
+   //main-ctrl+space
+  //syso-ctrl+space
+ //save-ctrl+s
+	public static void main(String[] args) {
+		System.out.println("welcome to testleaf 2023");
+	}
+}
